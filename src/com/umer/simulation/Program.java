669b4527b71a2641b98439bba75a875157485599
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-14 21:52:02 +0100

    Feature: Code clean-up

--- src/com/umer/simulation/Program.java
+++ src/com/umer/simulation/Program.java
@@ -1,19 +1,16 @@
 package com.umer.simulation;
 
-import java.util.Scanner;
-
-
 public class Program {
 
 	public static void main(String[] args) throws Exception {
-		
+
 //		Scanner input = new Scanner(System.in);
 //		System.out.println("Please enter your name:");
 //		String personName = input.nextLine();
 //		
 //		System.out.println("Hi, "+personName);
-		
-		Simulation simulation=new Simulation();
+
+		Simulation simulation = new Simulation();
 		simulation.run();
 	}
 }
