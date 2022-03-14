cf1ec437b5af12f2ecafa62a3366955a45f109fd
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-14 21:51:25 +0100

    Feature: Created a basic implementation of simulation which flips a coin randomly.

--- src/com/umer/simulation/Program.java
+++ src/com/umer/simulation/Program.java
@@ -7,10 +7,13 @@ public class Program {
 
 	public static void main(String[] args) throws Exception {
 		
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter your name:");
-		String personName = input.nextLine();
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("Please enter your name:");
+//		String personName = input.nextLine();
+//		
+//		System.out.println("Hi, "+personName);
 		
-		System.out.println("Hi, "+personName);
+		Simulation simulation=new Simulation();
+		simulation.run();
 	}
 }
--- src/com/umer/simulation/Simulation.java
+++ src/com/umer/simulation/Simulation.java
@@ -1,11 +1,19 @@
 package com.umer.simulation;
 
+import java.util.Random;
+
 public class Simulation {
+	Random rnd = new Random();
 
 	public void run() {
-		/* flip a coin and return 
-		 * heads 50% of the time and 
-		 * tails 50% of the time. */
-		
+		for (int i = 0; i < 100; i++) {
+			/*
+			 * flip a coin and 
+			 * return heads 50% of the time and 
+			 * tails 50% of the time.
+			 */
+			String result = this.rnd.nextDouble() <= 0.5 ? "Heads" : "Tails";
+			System.out.println(result);
+		}
 	}
 }
