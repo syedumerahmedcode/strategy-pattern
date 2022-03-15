5d2d1221188f70082bd401713595ec186ea0a161
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-15 21:59:21 +0100

    Feature: Taking user inputs now for the number of runs for the simulation and the initial seed.

--- src/com/umer/simulation/Program.java
+++ src/com/umer/simulation/Program.java
@@ -1,5 +1,7 @@
 package com.umer.simulation;
 
+import java.util.Scanner;
+
 import com.umer.simulation.writer.CsvWriter;
 import com.umer.simulation.writer.Writer;
 
@@ -7,19 +9,21 @@ public class Program {
 
 	public static void main(String[] args) throws Exception {
 
+		Scanner input = new Scanner(System.in);
 		
-		
-		int numberOfRuns=10;
-		long seed=2L;
-//		Scanner input = new Scanner(System.in);
-//		System.out.println("Please enter your name:");
-//		String personName = input.nextLine();
-//		
-//		System.out.println("Hi, "+personName);
-//		Writer writer=new ConsoleWriter();
+		System.out.println("Please number of runs:");
+		int numberOfRuns = Integer.parseInt(input.nextLine());
+
+		System.out.println("Please initial seed value:");
+		long seed = Long.parseUnsignedLong(input.nextLine());
+
+		//		Writer writer=new ConsoleWriter();
 		String path = "output.txt";
 		Writer writer = new CsvWriter(path);
+		
 		Simulation simulation = new Simulation(numberOfRuns, seed, writer);
 		simulation.run();
+		
+		input.close();
 	}
 }
